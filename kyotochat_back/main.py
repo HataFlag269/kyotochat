5a0996f2e913bb0898d17afeec80ab9ef4aa112f
Your Name <flaghatarealcode@gmail.com>
2023-12-02 13:32:43 +0900

    first commit

---
+++ kyotochat_back/main.py
@@ -0,0 +1,69 @@
+from fastapi import BackgroundTasks, FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from models import User, UserCredentials, SessionLocal, DATABASE_URL
+from fastapi import BackgroundTasks, FastAPI, Request, HTTPException, status
+from models import User, UserCredentials, SessionLocal, DATABASE_URL
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.staticfiles import StaticFiles
+from passlib.hash import bcrypt
+from fastapi.middleware.cors import CORSMiddleware
+from databases import Database
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session, joinedload
+import sqlalchemy
+
+app = FastAPI()
+database = Database(DATABASE_URL, force_rollback=True)
+metadata = sqlalchemy.MetaData()
+Base = declarative_base()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def root():
+    return await app.handle(request=request)
+
+
+@app.get("/a")
+async def a():
+    return {"aaa"}
+
+
+@app.post("/login")
+async def login(
+    request: Request,
+    credentials: UserCredentials,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(get_db),
+):
+    email = credentials.email
+    password = credentials.password
+    user = (
+        db.query(User)
+        .options(joinedload(User.chat_logs))
+        .filter(User.email == email)
+        .first()
+    )
+    if user and bcrypt.verify(password, user.password):
+        background_tasks.add_task(some_background_task, user.email)
+        return {"msg": "Login successful"}
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
+    )
---
+++ kyotochat_back/models.py
@@ -0,0 +1,47 @@
+from typing import List, Optional
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from passlib.hash import bcrypt
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, create_engine
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker, joinedload
+
+DATABASE_URL = "mysql+aiomysql://root:xxx@localhost/kyotoChat"
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    email = Column(String, index=True)
+    password = Column(String)
+    registration_date = Column(DateTime)
+    birthday = Column(DateTime)
+    gender = Column(String)
+    age = Column(Integer)
+    chat_logs = relationship("ChatLog", back_populates="user", lazy="selectin")
+
+    def verify_password(self, password: str):
+        return bcrypt.verify(password, self.password)
+
+class Character(Base):
+    __tablename__ = "characters"
+    character_id = Column(Integer, primary_key=True, index=True)
+    character_name = Column(String)
+    chat_logs = relationship("ChatLog", back_populates="character", lazy="selectin")
+
+class ChatLog(Base):
+    __tablename__ = "chat_logs"
+    log_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    character_id = Column(Integer, ForeignKey("characters.character_id"))
+    message = Column(Text)
+    send_datetime = Column(DateTime)
+
+    user = relationship("User", back_populates="chat_logs")
+    character = relationship("Character", back_populates="chat_logs")
+
+class UserCredentials(BaseModel):
+    email: str
+    password: str
